369b6f52fd2860a222973788e27f1278950144dc
chris <chris@mail.com>
2023-08-05 17:43:59 +0200

    V0.1

---
+++ javascript.js
@@ -0,0 +1,33 @@
+
+let listeMots=[ "cachalot", "petunia","serviette"];
+let listeMots2=[ "Pas de panique !", "La vie", "l’univers et le reste"];
+let score=0
+let liste;
+let choix=""
+
+//Demandez à l’utilisateur s’il veut la liste de mots ou la liste de phrases. 
+do {
+    choix=prompt(`Veuillez Faire un choix entre mots Ou phrase ` )
+
+    
+} 
+while (choix !== "mots" && choix !== "phrase")
+
+//Répétez la question tant que l’utilisateur n’a pas écrit “mots” ou “phrases”.
+if (choix=="mots") {
+    liste=[...listeMots]
+}
+if (choix=="phrase") {
+    liste=[...listeMots2]
+}
+
+
+for (let i = 0; i < liste.length; i++) {
+    mots=prompt("veuillez Entre " + liste[i])
+    
+    if (mots==liste[i]) {
+        score+=1
+    }
+    console.log(`votre Scores! = ${score}"/" ${i +1}`);
+}
+alert(`Total Scores===>${score}` )
