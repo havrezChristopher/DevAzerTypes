28330d46b67ca4e9e99a28681f4620a4921c96cf
chris <chris@mail.com>
2023-08-06 00:09:08 +0200

    ExerciesDom

--- javascript.js
+++ javascript.js
@@ -1,5 +1,23 @@
 
- lancerJeu()
+// lancerJeu()
 
-            
+          // Sélectionnez avec la méthode getElementById : 
+
+// l’input dans lequel le joueur va écrire son texte ; 
+let inputEcriture = document.getElementById("inputEcriture")
+// le bouton de validation.
+let btnValiderMot = document.getElementById("btnValiderMot")
+// Sélectionnez avec la méthode querySelector : 
+// l’endroit où le mot proposé sera affiché ;
+let zoneProposition =document.querySelector(".zoneProposition")
+
+// l’endroit où le score sera affiché. 
+let zoneScore =document.querySelector(".zoneScore span")
+// Sélectionnez avec la méthode querySelectorAll : 
+
+// les boutons radio de choix. 
+let listeoptionSource =document.querySelectorAll(".optionSource input ")
+for (let i = 0; i < listeoptionSource.length; i++) {
+    console.log(listeoptionSource[i]);
+}  
                 
