ad0d43cc88014ce3558a73956d5eaac3a9720188
chris <chris@mail.com>
2023-08-05 20:24:23 +0200

    Bordel OK!

--- javascript.js
+++ javascript.js
@@ -3,40 +3,35 @@ let score=0
 let liste;
 let choix=""
 // afficherResultat : cette fonction prend en paramètre le résultat et le nombre total de mots proposés, et affiche le résultat du joueur ; 
-function afficherResultat(score,liste) {
-    let message=`votre Scores! = ${score}"/" ${i +1}`
-        return message
-}
 
-// choisirPhrasesOuMots : cette fonction demande à l’utilisateur s’il veut jouer avec des phrases ou des mots.
-function choisirPhrasesOuMots(score,liste) {
+
+//Demandez à l’utilisateur s’il veut la liste de mots ou la liste de phrases. 
+do {
+    choix=prompt(`Veuillez Faire un choix entre mots Ou phrase ` )
     
-    //Demandez à l’utilisateur s’il veut la liste de mots ou la liste de phrases. 
-    do {
-        choix=prompt(`Veuillez Faire un choix entre mots Ou phrase ` )
-        
-        
-    } 
-    while (choix !== "mots" && choix !== "phrase")
     
-    switch (choix) {
-        case "mots":
-            liste=[...listeMots]
-            
-            break;
+} 
+while (choix !== "mots" && choix !== "phrase")
+
+switch (choix) {
+    case "mots":
+        liste=[...listeMots]
+        
+        break;
         case "phrase":
             liste=[...listeMots2]
-        default:
-            alert(" Fuck Off Ecrit Correctement !")
-            break;
-    }
-}
-
-
-for (let i = 0; i < liste.length; i++) {
-    mots=prompt("veuillez Entre " + liste[i])
-    
-    if (mots==liste[i]) {
-        score+=1
-    }
-}
+            default:
+                alert(" Fuck Off Ecrit Correctement !")
+                break;
+                
+                
+                }
+            
+                for (let i = 0; i < liste.length; i++) {
+                    mots=prompt("veuillez Entre " + liste[i])
+                    
+                    if (mots==liste[i]) {
+                        score+=1
+                    }
+                    console.log(`votre Scores! = ${score}"/" ${i +1}`);
+                }
